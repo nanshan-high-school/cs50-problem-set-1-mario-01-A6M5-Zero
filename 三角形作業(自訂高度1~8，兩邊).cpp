c1fa977ead993a65db4381acbe08d2a466546dea
A6M5-Zero <43640950+A6M5-Zero@users.noreply.github.com>
2018-10-18 19:51:19 +0800

    Update 三角形作業(自訂高度1~8，兩邊).cpp
    美化排版

--- 三角形作業(自訂高度1~8，兩邊).cpp
+++ 三角形作業(自訂高度1~8，兩邊).cpp
@@ -5,19 +5,19 @@ int main() {
 int H = 0;
 cout << "輸入高度:";
 cin >> H;
-if (1<=H && H <=8) {
-  for (int a = 0;a < H;a++) {
-    for (int b = a+1;b < H;b++) {
+  if (1<=H && H <=8) {
+    for (int a = 0;a < H;a++) {
+      for (int b = a+1;b < H;b++) {
+        cout << " ";
+      }
+      for (int c = H-1-a;c < H;c++) {
+        cout << "#";
+      }
       cout << " ";
+      for (int d = H-a;d < H;d++) {
+        cout << "#";
+      }
+      cout << "#\n";
     }
-    for (int c = H-1-a;c < H;c++) {
-      cout << "#";
-    }
-    cout << " ";
-    for (int d = H-a;d < H;d++) {
-      cout << "#";
-    }
-    cout << "#\n";
-  }
-} else {cout << "不在範圍內";} 
+  } else {cout << "不在範圍內";} 
 }
